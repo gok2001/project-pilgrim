52c67db8dea34d55948c7ad35864f0418fb92bf8
Guilherme Orige Kernbichler <guilhermeokern@gmail.com>
2025-10-25 11:38:10 -0300

    feat(player): add basic movement, jump and ground detection

---
+++ scripts/player.gd
@@ -0,0 +1,34 @@
+extends CharacterBody2D
+
+
+var gravity = ProjectSettings.get_setting('physics/2d/default_gravity')
+const WALK_SPEED = 300.0
+const JUMP_VELOCITY = -500.0
+
+
+func _physics_process(delta: float) -> void:
+	velocity.y += delta * gravity
+	
+	if Input.is_action_just_pressed('jump') and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	
+	var direction = Input.get_axis('left', 'right')
+	velocity.x = direction * WALK_SPEED
+	
+	move_and_slide()
+	#if not is_on_floor():
+		#velocity += get_gravity() * delta
+#
+	## Handle jump.
+	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		#velocity.y = JUMP_VELOCITY
+#
+	## Get the input direction and handle the movement/deceleration.
+	## As good practice, you should replace UI actions with custom gameplay actions.
+	#var direction := Input.get_axis("ui_left", "ui_right")
+	#if direction:
+		#velocity.x = direction * SPEED
+	#else:
+		#velocity.x = move_toward(velocity.x, 0, SPEED)
+#
+	#move_and_slide()
